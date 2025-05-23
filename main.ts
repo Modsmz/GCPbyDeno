a43708b56fe748bcda3929bd655e1e4c6d3484a2
Modsmz <channeltv1221@gmail.com>
2025-05-23 11:06:38 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { exists } from "https://deno.land/std/fs/exists.ts"; // Import exists function
 
-const envUUID = Deno.env.get('UUID') || 'e5185305-1984-4084-81e0-f77271159c62';
+const envUUID = Deno.env.get('UUID') || 'c475b1eb-61e5-46ad-9db4-434067071be7';
 const proxyIP = Deno.env.get('PROXYIP') || '';
 const credit = Deno.env.get('CREDIT') || 'DenoBy-ModsBots';
 
